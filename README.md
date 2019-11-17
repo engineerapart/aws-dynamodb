95bd947f6ae3ad8a2d81699977cb8ce9968a0fe8
Barry Steyn <bsteyn@linkedin.com>
2019-11-16 21:05:07 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
 # aws-dynamodb
-Fork for aws dynamodb until maintainer get's of their ass and merges things
+
+Easily provision AWS DynamoDB tables using [Serverless Components](https://github.com/serverless/components).
+
+&nbsp;
+
+1. [Install](#1-install)
+2. [Create](#2-create)
+3. [Configure](#3-configure)
+4. [Deploy](#4-deploy)
+
+&nbsp;
+
+### 1. Install
+
+```shell
+$ npm install -g serverless
+```
+
+### 2. Create
+
+Just create a `serverless.yml` file
+
+```shell
+$ touch serverless.yml
+$ touch .env      # your AWS api keys
+```
+
+```
+# .env
+AWS_ACCESS_KEY_ID=XXX
+AWS_SECRET_ACCESS_KEY=XXX
+```
+
+### 3. Configure
+
+```yml
+# serverless.yml
+
+myTable:
+  component: '@serverless/aws-dynamodb'
+  inputs:
+    name: nameOfTable # optional
+    attributeDefinitions:
+      - AttributeName: id
+        AttributeType: S
+    keySchema:
+      - AttributeName: id
+        KeyType: HASH
+    region: us-east-1
+```
+
+With globalSecondaryIndexes and/or localSecondaryIndexes
+
+```yml
+# serverless.yml
+
+myTable:
+  component: '@serverless/aws-dynamodb'
+  inputs:
+    name: nameOfTable # optional
+    attributeDefinitions:
+      - AttributeName: id
+        AttributeType: S
+      - AttributeName: attribute1
+        AttributeType: N
+      - AttributeName: attribute2
+        AttributeType: S
+    keySchema:
+      - AttributeName: id
+        KeyType: HASH
+      - AttributeName: attribute1
+        KeyType: RANGE
+    localSecondaryIndexes:
+      - IndexName: 'myLocalSecondaryIndex'
+        KeySchema:
+         - AttributeName: id
+           KeyType: HASH
+         - AttributeName: attribute2
+           KeyType: RANGE
+        Projection:
+           ProjectionType: 'KEYS_ONLY'
+    globalSecondaryIndexes:
+      - IndexName: 'myGlobalSecondaryIndex'
+        KeySchema:
+         - AttributeName: attribute2
+           KeyType: HASH
+        Projection:
+           ProjectionType: 'KEYS_ONLY'
+    region: us-east-1
+```
+
+The following applies to indexes:
+
+ 1. LocalIndexes can only be created upon table creation. There is no way to update them and/or create them other than at table creation.
+ 2. GlobalSecondaryIndexes can be created and removed during and after table creation. During an update, only one create and delete can happen at a time.
+ 3. This component uses [PAY_PER_REQUEST](https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_BillingModeSummary.html), which makes any throughput update redundant, including for GlobalSecondaryIndexes.
+
+### 4. Deploy
+
+```shell
+$ serverless
+```
+
+&nbsp;
+
+### New to Components?
+
+Checkout the [Serverless Components](https://github.com/serverless/components) repo for more information.
---
+++ serverless.js
@@ -0,0 +1,144 @@
+const { mergeDeepRight, pick, equals } = require('ramda')
+const AWS = require('aws-sdk')
+const { Component } = require('@serverless/core')
+const { createTable, deleteTable, describeTable, updateTable, configChanged } = require('./utils')
+
+const outputsList = ['name', 'arn', 'region']
+
+const defaults = {
+  attributeDefinitions: [
+    {
+      AttributeName: 'id',
+      AttributeType: 'S'
+    }
+  ],
+  keySchema: [
+    {
+      AttributeName: 'id',
+      KeyType: 'HASH'
+    }
+  ],
+  globalSecondaryIndexes: [],
+  name: false,
+  region: 'us-east-1'
+}
+
+const setTableName = (component, inputs, config) => {
+  const generatedName = inputs.name || component.context.resourceId()
+  const hasDeployedBefore = 'nameInput' in component.state
+  const givenNameHasNotChanged =
+    component.state.nameInput && component.state.nameInput === inputs.name
+  const bothLastAndCurrentDeployHaveNoNameDefined = !component.state.nameInput && !inputs.name
+
+  config.name =
+    hasDeployedBefore && (givenNameHasNotChanged || bothLastAndCurrentDeployHaveNoNameDefined)
+      ? component.state.name
+      : generatedName
+
+  component.state.nameInput = inputs.name || false
+}
+
+class AwsDynamoDb extends Component {
+  async default(inputs = {}) {
+    this.context.status('Deploying')
+    const config = mergeDeepRight(defaults, inputs)
+
+    this.context.debug(
+      `Starting deployment of table ${config.name} in the ${config.region} region.`
+    )
+
+    const dynamodb = new AWS.DynamoDB({
+      region: config.region,
+      credentials: this.context.credentials.aws
+    })
+
+    this.context.debug(
+      `Checking if table ${config.name} already exists in the ${config.region} region.`
+    )
+
+    setTableName(this, inputs, config)
+
+    const prevTable = await describeTable({ dynamodb, name: this.state.name })
+
+    if (!prevTable) {
+      this.context.status('Creating')
+      this.context.debug(`Table ${config.name} does not exist. Creating...`)
+
+      config.arn = await createTable({ dynamodb, ...config })
+    } else {
+      this.context.debug(`Table ${config.name} already exists. Comparing config changes...`)
+
+      config.arn = prevTable.arn
+
+      if (configChanged(prevTable, config)) {
+        this.context.status('Updating')
+        this.context.debug(`Config changed for table ${config.name}. Updating...`)
+
+        if (!equals(prevTable.name, config.name)) {
+          // If "delete: false", don't delete the table
+          if (config.delete === false) {
+            throw new Error(
+              `You're attempting to change your table name from ${this.state.name} to ${config.name} which will result in you deleting your table, but you've specified the "delete" input to "false" which prevents your original table from being deleted.`
+            )
+          }
+          await deleteTable({ dynamodb, name: prevTable.name })
+          config.arn = await createTable({ dynamodb, ...config })
+        } else {
+          const prevGlobalSecondaryIndexes = prevTable.globalSecondaryIndexes || []
+          await updateTable.call(this, { dynamodb, prevGlobalSecondaryIndexes, ...config })
+        }
+      }
+    }
+
+    this.context.debug(
+      `Table ${config.name} was successfully deployed to the ${config.region} region.`
+    )
+
+    this.state.arn = config.arn
+    this.state.name = config.name
+    this.state.region = config.region
+    this.state.delete = config.delete === false ? config.delete : true
+    await this.save()
+
+    const outputs = pick(outputsList, config)
+
+    return outputs
+  }
+
+  async remove(inputs = {}) {
+    this.context.status('Removing')
+
+    // If "delete: false", don't delete the table, and warn instead
+    if (this.state.delete === false) {
+      this.context.debug(`Skipping table removal because "delete" is set to "false".`)
+      return {}
+    }
+
+    const { name, region } = this.state
+
+    if (!name) {
+      this.context.debug(`Aborting removal. Table name not found in state.`)
+      return
+    }
+
+    const dynamodb = new AWS.DynamoDB({
+      region,
+      credentials: this.context.credentials.aws
+    })
+
+    this.context.debug(`Removing table ${name} from the ${region} region.`)
+
+    await deleteTable({ dynamodb, name })
+
+    const outputs = pick(outputsList, this.state)
+
+    this.context.debug(`Table ${name} was successfully removed from the ${region} region.`)
+
+    this.state = {}
+    await this.save()
+
+    return outputs
+  }
+}
+
+module.exports = AwsDynamoDb
